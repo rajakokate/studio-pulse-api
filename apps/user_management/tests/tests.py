fd1cc52c3311bffb158c401c492c29e620ba2f32
bhupendraingre <bhupendra.ingre@gmail.com>
2025-07-09 22:58:31 +0530

    Update tests.py

--- apps/user_management/tests/tests.py
+++ apps/user_management/tests/tests.py
@@ -4,5 +4,5 @@ from django.urls import reverse
 class HealthCheckTest(TestCase):
     def test_health_check(self):
         response = self.client.get(reverse('health-check'))  # Make sure this URL exists in your app
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"status": "healthy"})  # Expected response
